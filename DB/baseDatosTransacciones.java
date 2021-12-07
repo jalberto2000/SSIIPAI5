b7a77eb24d608a28fd401399625e63e0268e93e2
JuanAlberto <albertomarin14@hotmail.com>
2021-12-07 17:49:24 +0100

    añadida base de datos

---
+++ DB/baseDatosTransacciones.java
@@ -0,0 +1,44 @@
+package basedatos;
+
+import java.sql.*;
+
+public class baseDatosTransacciones {
+
+    public static void insertTransaction(String transactions,Boolean validity){
+        // Aqui se pone la ruta de la base de datos
+        String jdbcUrl = "jdbc:sqlite:.\\transactions.db";
+        Integer b = 0;
+        if (validity){
+            b = 1;
+        }
+        try {
+            Connection connection = DriverManager.getConnection(jdbcUrl);
+            String sql = String.format("INSERT INTO transactions (transactionId,transactions,validity) VALUES (NULL,'%s',%d)",transactions,b);
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+        } catch (SQLException e) {
+            System.out.println("Error insert to SQLite database");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main ( String args[] ) {
+        String jdbcUrl = "jdbc:sqlite:.\\transactions.db";
+        //insertTransaction("transaccionValida", true);
+        try {
+            Connection connection = DriverManager.getConnection(jdbcUrl);
+            String sql = "SELECT * FROM transactions";
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+            while (result.next()) {
+                Integer transactionId = result.getInt("transactionId");
+                String transactions = result.getString("transactions");
+                Boolean validity = result.getBoolean("validity");
+                System.out.println(transactionId + " | " + transactions + " | " + validity);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error connecting to SQLite database");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ DB/baseDatosUsuarios.java
@@ -0,0 +1,56 @@
+package basedatos;
+
+import java.sql.*;
+
+public class baseDatosUsuarios {
+    public static String getKey(String user){
+        // Aqui se pone la ruta de la base de datos
+        String jdbcUrl = "jdbc:sqlite:.\\users.db";
+        try {
+            Connection connection = DriverManager.getConnection(jdbcUrl);
+            String sql = String.format("SELECT key FROM users WHERE user = '%s'",user);
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+            String keyres = result.getString("key");
+            return keyres;
+        } catch (SQLException e) {
+            System.out.println("Error insert to SQLite database");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void insertUser(String user,String key){
+        // Aqui se pone la ruta de la base de datos
+        String jdbcUrl = "jdbc:sqlite:.\\users.db";
+        try {
+            Connection connection = DriverManager.getConnection(jdbcUrl);
+            String sql = String.format("INSERT INTO users (userId,user,key) VALUES (NULL,'%s','%s')",user,key);
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+        } catch (SQLException e) {
+            System.out.println("Error insert to SQLite database");
+            e.printStackTrace();
+        }
+    }
+    public static void main ( String args[] ) {
+        String jdbcUrl = "jdbc:sqlite:.\\users.db";
+        //insertUser("Jesse","test");
+        try {
+            Connection connection = DriverManager.getConnection(jdbcUrl);
+            String sql = "SELECT * FROM users";
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+            while (result.next()){
+                Integer userId = result.getInt("userId");
+                String user = result.getString("user");
+                String key = result.getString("key");
+                System.out.println(userId + " | " + user + " | " + key);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error connecting to SQLite database");
+            e.printStackTrace();
+        }
+
+    }
+}
